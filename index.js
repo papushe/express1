831469c3e846744302da23f3bd59f2cf0ec68ffc
Amit Papushe Shely <papushe9@gmail.com>
2017-05-06 19:18:43 +0300

    final

--- index.js
+++ index.js
@@ -16,28 +16,30 @@ app.use('/assets', express.static(`${__dirname}/public`));
 
 
 app.get('/', function (req, res) {
+    res.set('header-One', 'Home');
     res.send(`
     <!DOCTYPE html>
     <html>
     <head>
-        <link href=assets/style.css rel=stylesheet>
+        <link href="assets/style.css" rel=stylesheet>
+        <link rel="icon" href="assets/apple.png">
         <title>New Movies</title>
     </head>
     <body>
         <h1>My VOD - New Movies</h1>
         <div>
             <h2>All movies</h2>
-            <a  href="https://express1vod.herokuapp.com/getAllMoviesData">https://express1vod.herokuapp.com/getAllMoviesData</a>
+            <a target="_blank" href="https://express1vod.herokuapp.com/getAllMoviesData">https://express1vod.herokuapp.com/getAllMoviesData</a>
             <p>You will see all the data json<p/>
         </div>
         <div>
             <h2>Two cuts by get</h2>
-            <a href="https://express1vod.herokuapp.com/getMoviesName/action/movie">https://express1vod.herokuapp.com/getMoviesName/action/movie</a>
+            <a target="_blank" href="https://express1vod.herokuapp.com/getMoviesName/action/movie">https://express1vod.herokuapp.com/getMoviesName/action/movie</a>
             <p>You will see all the data json after 2 cuts genres(action) and date(jan)<p/>
         </div>
         <div>
             <h2>One cut by post</h2>
-            <a href="https://express1vod.herokuapp.com/getAllMoviesData">https://express1vod.herokuapp.com/getAllMoviesData</a>
+            <a target="_blank" href="https://express1vod.herokuapp.com/getDataByName">https://express1vod.herokuapp.com/getDataByName</a>
             <p>check it out on the REST client<p/>
         </div>
     </body>
@@ -45,10 +47,12 @@ app.get('/', function (req, res) {
 });
 
 app.get('/getAllMoviesData', (req, res)=> {
+    res.set('header-Two', 'All data');
     res.status(200).json({"movies Data": allMovies}); // get all data movies by getAllMoviesData() function
 });
 
 app.get('/getMoviesName/:movies_genres/:movies_type', (req, res)=> {
+    res.set('header-Three', 'Two cuts');
     genresAndType = modules.getGenresAndType(req.params.movies_genres, req.params.movies_type); // create var of getGenresAndType function with 2 parameters
     if(genresAndType.length == 0){ // if the array is empty
         res.status(200).json({error: "wrong input, try again"}); // error
@@ -58,6 +62,7 @@ app.get('/getMoviesName/:movies_genres/:movies_type', (req, res)=> {
 });
 
 app.post('/getDataByName/', (req, res)=> {
+    res.set('header-Two', 'One data, post');
     let movie = req.body.date, // create bodyParser var
         byName = modules.getDataByName(movie); // insert it to the function getDataByName
     if(byName.length == 0){ // if the array is empty
